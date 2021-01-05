386a1c5a6232ac9f6b375011bf0b9561d98cecba
sarazafe <sara@blunder.es>
2021-01-05 19:45:42 +0100

    feat: Load initial data in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, {Component, Fragment} from "react";
+import {connect} from 'react-redux';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import LoadingBar from 'react-redux-loading';
 import Dashboard from "./Dashboard";
 import QuestionDetails from "./QuestionDetails";
 import NewQuestion from "./NewQuestion";
 import LeaderBoard from "./LeaderBoard";
 import Login from "./Login";
+import {handleInitialData} from "../common/common";
 
-function App() {
-	return (
-		<Router>
-			<div className='container'>
-				<div>
-					<Route path='/' exact component={Dashboard} />
-					<Route path='/login' exact component={Login} />
-					<Route path='/questions/:question_id' component={QuestionDetails} />
-					<Route path='/add' component={NewQuestion} />
-					<Route path='/leaderboard' component={LeaderBoard} />
-				</div>
-			</div>
-		</Router>
-	);
+class App extends Component {
+	componentDidMount() {
+		this.props.dispatch(handleInitialData());
+	}
+
+	render() {
+		return (
+			<Router>
+				<Fragment>
+					<LoadingBar/>
+					<div className='container'>
+						<div>
+							<Route path='/' exact component={Dashboard}/>
+							<Route path='/login' exact component={Login}/>
+							<Route path='/questions/:question_id' component={QuestionDetails}/>
+							<Route path='/add' component={NewQuestion}/>
+							<Route path='/leaderboard' component={LeaderBoard}/>
+						</div>
+					</div>
+				</Fragment>
+			</Router>
+		);
+	}
 }
 
-export default App;
+function mapStateToProps({users}) {
+	return {
+		loading: users === null || users === undefined
+	};
+}
+
+export default connect(mapStateToProps)(App);
