16ea2db3e083e736c78e2bb9995deac97a3655de
sarazafe <sara@blunder.es>
2021-01-08 20:39:33 +0100

    feat: Add a header component This component has the title, the information about logged user and logout button

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+import {logoutUser} from "../actions/loggedUser";
+
+/**
+ * Component for the header
+ */
+class Header extends Component {
+	/**
+	 * Logs the user out of session
+	 */
+	logoutUser = ()=> {
+		this.props.dispatch(logoutUser());
+	};
+
+	render() {
+		const {name, avatarURL} = this.props.loggedUser;
+		return (
+			<div className='header'>
+				<div className='header-title'>Would you rather?</div>
+				<div className='header-logged-user'>
+					<div className='header-logged-user-avatar'><img src={avatarURL} alt={`${name}'s avatar`}/></div>
+					<div className='header-logged-user-name'>{name}</div>
+					<div className='header-logged-user-logout'>
+						<button onClick={this.logoutUser}>Logout</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps({loggedUser}) {
+	return {
+		loggedUser,
+	};
+}
+
+export default connect(mapStateToProps)(Header);
