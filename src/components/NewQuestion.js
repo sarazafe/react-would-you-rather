fb3a646c33b5e3fd5a2842295a55152ca254ec3c
sarazafe <sara@blunder.es>
2021-01-08 21:37:46 +0100

    feat: Add the form to save a new question

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,24 +1,75 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
 import Header from "./Header";
 import {Nav} from "./Nav";
+import {handleAddNewQuestion} from "../actions/questions";
 
 /**
  * Component for new question page
  */
 class NewQuestion extends Component {
+	state = {
+		optionOne: '',
+		optionTwo: '',
+	};
+
+	updateOptionOne = event=>{
+		this.setState({
+			optionOne: event.target.value,
+		})
+	};
+
+	updateOptionTwo = event=>{
+		this.setState({
+			optionTwo: event.target.value,
+		})
+	};
+
+	/**
+	 * Dispatches the action that add a new question to the state of the store
+	 * @param event - the submit event
+	 */
+	addNewQuestion = event=> {
+		event.preventDefault();
+		const {optionOne, optionTwo} = this.state;
+		const {loggedUser, dispatch} = this.props;
+		dispatch(handleAddNewQuestion({
+			optionOneText: optionOne,
+			optionTwoText: optionTwo,
+			author: loggedUser.id,
+		}));
+		this.setState({
+			optionOne: '',
+			optionTwo: '',
+		});
+	};
+
 	render() {
 		const {loggedUser} = this.props;
 		if (!loggedUser) {
 			return <Redirect push to='login'/>;
 		}
 
+		const {optionOne, optionTwo} = this.state;
 		return (
 			<div>
 				<Header/>
 				<Nav/>
-				NewQuestion page
+				<div className='new-question'>
+					<div className='new-question-title'>Would you rather?</div>
+					<form className='new question-form' onSubmit={this.addNewQuestion}>
+						<div className='new-question-form-option'>
+							<input type='text' value={optionOne} onChange={this.updateOptionOne}/>
+						</div>
+						<div className='new-question-form-or'>or</div>
+						<div className='new-question-form-option'>
+							<input type='text' value={optionTwo} onChange={this.updateOptionTwo}/>
+						</div>
+
+						<button type='submit' disabled={!optionOne || !optionTwo}>Add new question</button>
+					</form>
+				</div>
 			</div>
 		);
 	}
