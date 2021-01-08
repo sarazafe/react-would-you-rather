dd94d468fd9baf35d1f53997b9dfb6ddc30dcf68
sarazafe <sara@blunder.es>
2021-01-08 21:37:35 +0100

    feat: Add a handler for saving new questions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,3 +1,6 @@
+import {saveQuestion} from "../common/api";
+import { showLoading, hideLoading } from 'react-redux-loading';
+
 export const GET_QUESTIONS = 'GET_QUESTIONS';
 
 /**
@@ -8,5 +11,18 @@ export const getQuestions = (questions) => {
 	return {
 		type: GET_QUESTIONS,
 		questions,
+	}
+};
+
+/**
+ * Handler for adding a new question
+ * @param question - the question to be added
+ * @returns {function(*, *): Promise<unknown>}
+ */
+export const handleAddNewQuestion = (question) => {
+	return (dispatch) => {
+		dispatch(showLoading())
+		return saveQuestion(question)
+			.then(() => dispatch(hideLoading()))
 	}
 };
