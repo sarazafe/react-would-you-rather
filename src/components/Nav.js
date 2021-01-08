6c278030c482ce7fa0880b86482bb520d14529e8
sarazafe <sara@blunder.es>
2021-01-08 20:57:18 +0100

    feat: Created nav component
    This component has the following sections: home, new question and leader board

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+/**
+ * Navigation component
+ */
+export const Nav = () => {
+	return (
+		<nav className='nav'>
+			<ul>
+				<li>
+					<NavLink to='/' exact>
+						Home
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/add'>
+						New question
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/leaderboard'>
+						Leader board
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
