bcf3df9ffed899a132ab408ad0bba8fb4543c2d9
sarazafe <sara@blunder.es>
2021-01-12 21:11:15 +0100

    feat: Component to render poll details
    Created component that renders the details of a poll

---
+++ src/components/PollDetails.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {ProgressBar} from "react-step-progress-bar";
+
+/**
+ * Component for the details of a poll
+ * @param optionOne - first option
+ * @param optionTwo - second option
+ * @param totalOfVotes - total of votes
+ */
+export const PollDetails = ({optionOne, optionTwo, totalOfVotes}) => {
+	return (
+		<>
+			<div
+				className={`poll-details-option ${optionOne.answered ? 'poll-details-option--answered' : ''}`}>
+				<div className='poll-details-option-text'>
+					{optionOne.text}
+				</div>
+				<div className='poll-details-option-percentage'>
+					<ProgressBar percent={optionOne.percentage} text={`${optionOne.percentage}%`}/>
+				</div>
+				<div className='poll-details-option-votes'>
+					{optionOne.votes.length} out of {totalOfVotes} votes
+				</div>
+			</div>
+			<div
+				className={`poll-details-option ${optionTwo.answered ? 'poll-details-option--answered' : ''}`}>
+				<div className='poll-details-option-text'>
+					{optionTwo.text}
+				</div>
+				<div className='poll-details-option-percentage'>
+					<ProgressBar percent={optionTwo.percentage} text={`${optionTwo.percentage}%`}/>
+				</div>
+				<div className='poll-details-option-votes'>
+					{optionTwo.votes.length} out of {totalOfVotes} votes
+				</div>
+			</div>
+		</>
+	);
+};
+
+PollDetails.propTypes = {
+	optionOne: PropTypes.object.isRequired,
+	optionTwo: PropTypes.object.isRequired,
+	totalOfVotes: PropTypes.number.isRequired,
+};
