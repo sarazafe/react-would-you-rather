8c4bb31409a506c9efd96ade7ad89ee1b89f2d2b
sarazafe <sara@blunder.es>
2021-01-12 21:13:21 +0100

    feat: Component to answer a poll
    Component that renders a form to be able to answer a poll

---
+++ src/components/AnswerPoll.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+import {handleAnswerQuestion} from "../actions/questions";
+
+/**
+ * Component for form to answer a poll
+ */
+class AnswerPoll extends Component {
+	static propTypes = {
+		qid: PropTypes.string.isRequired,
+		optionOne: PropTypes.string.isRequired,
+		optionTwo: PropTypes.string.isRequired,
+	};
+
+	state = {
+		answer: '',
+	}
+
+	updateSelectedAnswer = event => {
+		this.setState({
+			answer: event.target.value,
+		});
+	}
+
+	/**
+	 * Dispatches the action that answers the poll and
+	 * updates the store with this new situation
+	 * @param event - the submit event
+	 */
+	answerPoll = event => {
+		event.preventDefault();
+		const {answer} = this.state;
+		const {loggedUser, dispatch, qid} = this.props;
+		dispatch(handleAnswerQuestion({
+			loggedUser,
+			answer,
+			qid,
+		}));
+	};
+
+	render() {
+		const {optionOne, optionTwo} = this.props;
+		return (
+			<form className='answer-poll-form' onSubmit={this.answerPoll}>
+				<div>
+					<input type="radio" id="optionOne" name="options" value="optionOne"
+					       onChange={this.updateSelectedAnswer}/>
+					<label htmlFor="optionOne">{optionOne}</label>
+				</div>
+				<div className='answer-poll-form-separator'>or</div>
+				<div>
+					<input type="radio" id="optionTwo" name="options" value="optionTwo"
+					       onChange={this.updateSelectedAnswer}/>
+					<label htmlFor="optionTwo">{optionTwo}</label>
+				</div>
+
+				<button type='submit'>Answer poll</button>
+			</form>
+		);
+	}
+}
+
+const mapStateToProps = ({loggedUser:{id}}) => {
+	return {
+		loggedUser: id,
+	};
+};
+
+export default connect(mapStateToProps)(AnswerPoll);
