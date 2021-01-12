886aaef8d6624c5fa676a5169e6ba1cece6df3c6
sarazafe <sara@blunder.es>
2021-01-12 21:17:24 +0100

    feat: Add all functionality to QuestionDetails page
    QuestionDetails page is in charge of showing the details of
    a poll depending on if logged user has answered this question or not

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
 import "react-step-progress-bar/styles.css";
-import { ProgressBar } from "react-step-progress-bar";
 import Header from "./Header";
 import {Nav} from "./Nav";
+import {PollDetails} from "./PollDetails";
+import AnswerPoll from "./AnswerPoll";
 
 /**
  * Component for question details page
@@ -15,8 +16,7 @@ class QuestionDetails extends Component {
 		if (!loggedUser) {
 			return <Redirect push to='/login'/>;
 		}
-		const {question: {optionOne, optionTwo, totalOfVotes}} = this.props;
-		const {name, avatarURL} = loggedUser;
+		const {question: {id, author: {name, avatarURL}, optionOne, optionTwo, totalOfVotes, unanswered}} = this.props;
 		return (
 			<div>
 				<Header/>
@@ -26,31 +26,16 @@ class QuestionDetails extends Component {
 						<img src={avatarURL} alt={`${name}'s avatar`}/>
 					</div>
 					<div className='question-details-poll'>
-						<div className='question-details-title'>
+						<div className='poll-details-title'>
 							{name} asks <span>Would you rather?</span>
 						</div>
-						<div className={`question-details-option ${optionOne.answered ? 'question-details-option--answered' : ''}`}>
-							<div className='question-details-option-text'>
-								{optionOne.text}
-							</div>
-							<div className='question-details-option-percentage'>
-								<ProgressBar percent={optionOne.percentage} text={`${optionOne.percentage}%`} />
-							</div>
-							<div className='question-details-option-votes'>
-								{optionOne.votes.length} out of {totalOfVotes} votes
-							</div>
-						</div>
-						<div className={`question-details-option ${optionTwo.answered ? 'question-details-option--answered' : ''}`}>
-							<div className='question-details-option-text'>
-								{optionTwo.text}
-							</div>
-							<div className='question-details-option-percentage'>
-								<ProgressBar percent={optionTwo.percentage} text={`${optionTwo.percentage}%`} />
-							</div>
-							<div className='question-details-option-votes'>
-								{optionTwo.votes.length} out of {totalOfVotes} votes
-							</div>
-						</div>
+						{
+							unanswered ? (
+									<AnswerPoll qid={id} optionOne={optionOne.text} optionTwo={optionTwo.text}/>
+								)
+								: <PollDetails optionOne={optionOne} optionTwo={optionTwo}
+								               totalOfVotes={totalOfVotes}/>
+						}
 					</div>
 				</div>
 			</div>
@@ -62,33 +47,56 @@ class QuestionDetails extends Component {
  * Gets all data of a question. This includes the total of votes, the percentage of votes per each option and
  * which question has answered the user in session if he/she has answered one of them
  * @param question - the question to get data from
+ * @param users - the map of users
  * @param loggedUser - the user in session
  * @returns the question with all needed data
  */
-const getQuestionData = (question, loggedUser) => {
-	const {optionOne, optionTwo} = question;
-	const totalOfVotes = question.optionOne.votes.length + question.optionTwo.votes.length;
+const getQuestionData = (question, users, loggedUser) => {
+	// TODO check if question exists or not -> 404 page
 
+	// Get author info
+	const {id, name, avatarURL} = users[question.author];
+
+	const questionData = {
+		...question,
+		author: {
+			id,
+			name,
+			avatarURL,
+		},
+	};
+
+	// Check if logged user has answered the poll
+	const {optionOne, optionTwo} = question;
+	if (!optionOne.votes.find(v => v === loggedUser.id) &&
+		!optionTwo.votes.find(v => v === loggedUser.id)) {
+		return {
+			...questionData,
+			unanswered: true,
+		}
+	}
+
+	const totalOfVotes = question.optionOne.votes.length + question.optionTwo.votes.length;
 	return {
-			...question,
-			optionOne: {
-				...optionOne,
-				percentage: (optionOne.votes.length / totalOfVotes) * 100,
-				answered: optionOne.votes.findIndex(v => v === loggedUser.id) >= 0
-			},
-			optionTwo: {
-				...optionTwo,
-				percentage: (optionTwo.votes.length / totalOfVotes) * 100,
-				answered: optionTwo.votes.findIndex(v => v === loggedUser.id) >= 0
-			},
-			totalOfVotes,
+		...questionData,
+		optionOne: {
+			...optionOne,
+			percentage: (optionOne.votes.length / totalOfVotes) * 100,
+			answered: optionOne.votes.findIndex(v => v === loggedUser.id) >= 0
+		},
+		optionTwo: {
+			...optionTwo,
+			percentage: (optionTwo.votes.length / totalOfVotes) * 100,
+			answered: optionTwo.votes.findIndex(v => v === loggedUser.id) >= 0
+		},
+		totalOfVotes,
 	};
 }
 
-const mapStateToProps = ({loggedUser, questions}, {match: {params: {question_id}}}) => {
+const mapStateToProps = ({loggedUser, users, questions}, {match: {params: {question_id}}}) => {
 	return {
 		loggedUser,
-		question: questions[question_id] && loggedUser ? getQuestionData(questions[question_id], loggedUser) : {},
+		question: questions[question_id] && loggedUser ? getQuestionData(questions[question_id], users, loggedUser) : {},
 	};
 };
 
