900e5ef5f5747c093bf8284da46ef6f388fedf32
sarazafe <sara@blunder.es>
2021-01-15 18:59:24 +0100

    feat: Style for PollDetails

--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {ProgressBar} from "react-step-progress-bar";
+import './PollDetails.css';
 
 /**
  * Component for the details of a poll
