93b18f744cea9e60ea3e1a0d63936a5bbc65de33
sarazafe <sara@blunder.es>
2021-01-15 18:58:24 +0100

    feature: New component QuestionCard
    This component will have a header with the name of the user,
    the avatar of the user and inside a children that will be customized

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './QuestionCard.css';
+
+/**
+ * Component that represents a question card. The structure will be:
+ * - the name of the user as title
+ * - the avatar of the user
+ * - a custom body
+ * @param name - the name of the user
+ * @param avatarURL - the url of user's avatar
+ * @param children - the content that will be render inside the body of the card
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const QuestionCard = ({name, avatarURL, children}) => {
+	return (
+		<div className='question-card'>
+			<div className='question-card-header'>
+				<span>{name} asks:</span>
+			</div>
+			<div className='question-card-body'>
+				<div className='question-card-body-avatar'>
+					<img src={avatarURL} alt={`${name}'s avatar`}/>
+				</div>
+				{children}
+			</div>
+		</div>);
+};
